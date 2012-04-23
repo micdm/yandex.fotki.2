45ebbac10db5ccf24f1b78a56db4a7041c74019a
Mic <mic-dm@mail.ru>
2012-04-23 23:38:21 +0700

    new: повторяем попытки, пока запрос не пройдет

--- src/dm_yf/http.py
+++ src/dm_yf/http.py
@@ -4,7 +4,8 @@ HTTP-загрузчик.
 @author: Mic, 2012
 '''
 
-from urllib2 import Request, urlopen
+from time import sleep
+from urllib2 import Request, urlopen, HTTPError
 
 from dm_yf.log import logger
 from dm_yf.oauth import OAuth
@@ -28,6 +29,9 @@ class HttpClient(object):
     HTTP-загрузчик.
     '''
     
+    # Интервал между повторами запроса (секунды):
+    RETRY_INTERVAL = 5
+    
     def _get_headers(self, headers):
         '''
         Возвращает заголовки для запроса (в частности, для авторизации).
@@ -37,8 +41,8 @@ class HttpClient(object):
             headers = {}
         headers['Authorization'] = 'OAuth %s'%OAuth.get_token()
         return headers
-    
-    def request(self, url, data=None, headers=None, method='GET'):
+
+    def _request(self, url, data, headers, method):
         '''
         Выполняет запрос и возвращает тело ответа.
         @param method: string
@@ -52,3 +56,19 @@ class HttpClient(object):
         response = urlopen(request).read()
         logger.debug('%s bytes loaded from url %s', len(response), url)
         return response
+    
+    def request(self, url, data=None, headers=None, method='GET'):
+        '''
+        Выполняет запрос и возвращает тело ответа.
+        Делает бесконечное число попыток, если запрос не проходит.
+        @param method: string
+        @param url: string
+        @param data: string
+        @return: string
+        '''
+        while True:
+            try:
+                return self._request(url, data, headers, method)
+            except HTTPError as e:
+                logger.warning('error occured during request: %s, retrying in %s seconds', e, self.RETRY_INTERVAL)
+                sleep(self.RETRY_INTERVAL)
